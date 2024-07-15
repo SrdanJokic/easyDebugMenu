3b3267d301487f478b2cc3080ca06b22a9d6e129
Srđan Jokić <41946771+SrdanJokic@users.noreply.github.com>
2024-07-15 22:54:44 +0200

    Rider - Headers.

--- components/HorizontalLayoutGroup.cs
+++ components/HorizontalLayoutGroup.cs
@@ -1,3 +1,10 @@
+// HorizontalLayoutGroup.cs
+// 
+// This script is licensed under the MIT License.
+// See the LICENSE file in the root of the repository for more details.
+// 
+// Copyright (c) 2024 Srdan Jokic
+
 namespace EasyDebugMenu.Components;
 
 public partial class HorizontalLayoutGroup : LayoutGroup
