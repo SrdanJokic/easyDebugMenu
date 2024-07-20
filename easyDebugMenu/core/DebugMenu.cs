f7d3d3f3ab849d5f19494f48f8b6fa1649807985
Srđan Jokić <41946771+SrdanJokic@users.noreply.github.com>
2024-07-20 09:41:59 +0200

    Main - Refactors.

--- easyDebugMenu/core/DebugMenu.cs
+++ easyDebugMenu/core/DebugMenu.cs
@@ -6,14 +6,18 @@
 // Copyright (c) 2024 Srdan Jokic
 
 using System;
+using System.Collections.Generic;
+using EasyDebugMenu.Components;
 using Godot;
 
-namespace EasyDebugMenu.Components;
+namespace EasyDebugMenu;
 
 public static class DebugMenu
 {
     public static event Action OnDisplayed;
     public static event Action OnHidden;
+
+    private static List<Group> _groups;
     
     // TODO: Add alignment options
     public static void Display(Node root)
@@ -30,6 +34,16 @@ public static class DebugMenu
         horizontal.CreateButton("Title", null);
         
         OnDisplayed?.Invoke();
+    }
+
+    private static void Draw()
+    {
+        
+    }
+    
+    public static void Add(Group group)
+    {
+        _groups.Add(group);
     }
 
     public static void Hide()
---
+++ samples/DebugMenuConfigurator.cs
@@ -0,0 +1,13 @@
+// DebugMenuConfigurator.cs
+// 
+// This script is licensed under the MIT License.
+// See the LICENSE file in the root of the repository for more details.
+// 
+// Copyright (c) 2024 Srdan Jokic
+
+namespace EasyDebugMenu.Samples;
+
+public class DebugMenuConfigurator
+{
+    
+}
